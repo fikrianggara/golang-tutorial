29aed0dd58603f35cc5b92202b325452cb89a81e
fikrianggara <fikriseptriananggara@gmail.com>
2022-11-09 11:23:15 +0700

    cdsacdas

--- booking-app/main.go
+++ booking-app/main.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 
 	"booking-app/hello"
+
+	"github.com/fikrianggara/golang-tutorial/booking-app/another-module/timestamp"
 )
 
 func main() {
 	fmt.Print(morestrings.ReverseRunes("!oG ,olleH"))
+	timestamp.GetTime()
 	hello.HelloInIndonesia()
 
 }
