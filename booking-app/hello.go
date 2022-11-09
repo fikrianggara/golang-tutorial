e0a04f30c95ed28d7f14c3c2a618f417355a6d26
fikrianggara <fikriseptriananggara@gmail.com>
2022-11-09 09:22:15 +0700

    test2

--- booking-app/hello.go
+++ booking-app/hello.go
@@ -1,7 +1,9 @@
 package main
 
 import "fmt"
-
+func Hello(){
+	fmt.Println("hello from booking-app package")
+}
 func init(){
 	fmt.Println("hello from hello.go")
 }
--- booking-app/main.go
+++ booking-app/main.go
@@ -1,9 +1,7 @@
 package main
 
 import "fmt"
-func hello(){
-	fmt.Println("hello from booking-app package")
-}
+
 func main() {
 	fmt.Print("hello world")
 }
